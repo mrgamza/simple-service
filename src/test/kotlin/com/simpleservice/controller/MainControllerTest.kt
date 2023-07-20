773f94e07a437cc7c4426d22c2a0f6d21480875a
Donghyuk <mrgamza@gmail.com>
2023-07-20 17:20:42 +0900

    Add test

--- src/test/kotlin/com/simpleservice/controller/MainControllerTest.kt
+++ src/test/kotlin/com/simpleservice/controller/MainControllerTest.kt
@@ -8,10 +8,18 @@ class MainControllerTest {
 
     @Test
     @DisplayName("Main test")
-    fun main() {
+    fun main_test() {
         val mainController = MainController()
         val result = mainController.index()
         assertEquals(result.statusCode.value(), 200)
         assertEquals(result.body, "Hello, simple-service")
+    }
+
+    @Test
+    @DisplayName("View test")
+    fun view_test() {
+        val mainController = MainController()
+        val result = mainController.view()
+        assertEquals(result, "main.html")
     }
 }
