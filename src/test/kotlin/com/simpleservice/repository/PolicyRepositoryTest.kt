eba399485567a62817b7f96497ee2c51ab0ac7d3
Donghyuk <mrgamza@gmail.com>
2023-07-24 10:00:00 +0900

    Add test

--- src/test/kotlin/com/simpleservice/repository/PolicyRepositoryTest.kt
+++ src/test/kotlin/com/simpleservice/repository/PolicyRepositoryTest.kt
@@ -1,10 +1,14 @@
 package com.simpleservice.repository
 
+import com.simpleservice.entity.Policy
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.sql.Timestamp
+import java.util.*
+import javax.persistence.Column
 
 @SpringBootTest
 class PolicyRepositoryTest {
@@ -14,12 +18,32 @@ class PolicyRepositoryTest {
 
     @Test
     @DisplayName("전체 policy가 조회되는지 확인")
-    fun saveMember() {
+    fun test_find_all_policy() {
         // given
         // Nothing
 
         // when
         val policies = policyRepository.findAll()
+
+        // then
+        Assertions.assertThat(policies).isNotNull
+    }
+
+    @Test
+    @DisplayName("policy를 저장한다.")
+    fun test_add_policy() {
+        // given
+        val policy = Policy(
+            title = "TEST",
+            comment = "TEST",
+            start = Date(),
+            end = Date(),
+            createdAt = Timestamp(System.currentTimeMillis()),
+            updatedAt = Timestamp(System.currentTimeMillis())
+        )
+
+        // when
+        val policies = policyRepository.save(policy)
 
         // then
         Assertions.assertThat(policies).isNotNull
