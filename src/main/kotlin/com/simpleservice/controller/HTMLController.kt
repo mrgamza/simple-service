ad6dcad55f9c60ab9da54e0b880f63d4499ed0db
Donghyuk <mrgamza@gmail.com>
2023-08-06 10:00:13 +0900

    add html

--- src/main/kotlin/com/simpleservice/controller/HTMLController.kt
+++ src/main/kotlin/com/simpleservice/controller/HTMLController.kt
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 @Controller
-class MainController {
+class HTMLController {
 
-    @ApiOperation(value = "Hello", notes = "Say Hello.")
-    @ApiResponses(
-        ApiResponse(code = 200, message = "OK")
-    )
-    @RequestMapping("/", method = [RequestMethod.GET])
-    @ResponseBody
-    fun index() = run {
-        ResponseEntity.ok("Hello, simple-service")
+    @RequestMapping("/", method = [RequestMethod.GET], produces = ["text/html"])
+    fun view() = run {
+        "main.html"
+    }
+
+    @RequestMapping("/policies", method = [RequestMethod.GET], produces = ["text/html"])
+    fun getPolicies() = run {
+        "get_policies.html"
     }
 }
