44a620a68d333805ab9e526fa90c3f73722c1148
Donghyuk <mrgamza@gmail.com>
2023-08-20 23:21:02 +0900

    modify main endpoint

--- src/main/kotlin/com/simpleservice/controller/MainController.kt
+++ src/main/kotlin/com/simpleservice/controller/MainController.kt
@@ -4,18 +4,16 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
-@Controller
+@RestController
 class MainController {
 
     @ApiOperation(value = "Hello", notes = "Say Hello.")
     @ApiResponses(
         ApiResponse(code = 200, message = "OK")
     )
-    @RequestMapping("/", method = [RequestMethod.GET])
-    @ResponseBody
+    @GetMapping("/")
     fun index() = run {
         ResponseEntity.ok("Hello, simple-service")
     }
